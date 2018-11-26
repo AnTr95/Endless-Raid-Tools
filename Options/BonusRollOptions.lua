9b74341e033df20e65e58ed9b89f4cee734b5d1b
alex riku <alexriku@outlook.com>
2018-11-27 01:58:52 +0200

    changed to EnRTLocals

--- Options/BonusRollOptions.lua
+++ Options/BonusRollOptions.lua
@@ -1,4 +1,4 @@
-local L = PGFinderLocals
+local L = EnRTLocals;
 
 EnRT_BonusRollOptions = CreateFrame("Frame", "EnRT_BonusRollOptionsFrame", InterfaceOptionsFramePanelContainer)
 EnRT_BonusRollOptions.name = "Bonus Roll Module"
--- Options/CalendarNotificationOptions.lua
+++ Options/CalendarNotificationOptions.lua
@@ -1,4 +1,4 @@
-local L = PGFinderLocals
+local L = EnRTLocals;
 
 EnRT_CalendarNotificationOptions = CreateFrame("Frame", "EnRT_CalendarNotificationOptionsFrame", InterfaceOptionsFramePanelContainer)
 EnRT_CalendarNotificationOptions.name = L.OPTIONS_CALENDARNOTIFICATION_TITLE
