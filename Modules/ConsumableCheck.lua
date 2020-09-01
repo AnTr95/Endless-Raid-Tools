ac83fa2c65f231406c7e4a2ea827203330c04c76
Ant <anton.ronsjo@gmail.com>
2020-09-02 01:20:33 +0200

    added armor kit to consumable check and remade the entire UI for it

--- Modules/ConsumableCheck.lua
+++ Modules/ConsumableCheck.lua
@@ -1,7 +1,88 @@
+local f2 = CreateFrame("Frame")--, nil, nil, BackdropTemplateMixin and "BackdropTemplate");
+f2:SetMovable(false);
+f2:EnableMouse(false);
+f2:SetFrameLevel(3);
+f2:SetFrameStrata("FULLSCREEN");
+f2:SetHeight(25);
+f2:SetWidth(250);
+--[[
+f2:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background", --Set the background and border textures
+	edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
+	tile = true, tileSize = 16, edgeSize = 16, 
+	insets = { left = 4, right = 4, top = 4, bottom = 4 }
+});
+f2:SetBackdropColor(0.3,0.3,0.3,0.6);
+f2:Hide();
+
+local texture = f2:CreateTexture();
+texture:SetTexture(0.5, 0.5, 0.5, 0.5);
+texture:SetAllPoints();
+
+local point, relativeTo, relativePoint, xOfs, yOfs = ReadyCheckFrameText:GetPoint();
+ReadyCheckFrameText:SetPoint(point, relativeTo, relativePoint, xOfs, yOfs+13);
+point, relativeTo, relativePoint, xOfs, yOfs = ReadyCheckFrameYesButton:GetPoint();
+ReadyCheckFrameYesButton:SetPoint(point, relativeTo, relativePoint, xOfs, yOfs-10);
+point, relativeTo, relativePoint, xOfs, yOfs = ReadyCheckFrameNoButton:GetPoint();
+ReadyCheckFrameNoButton:SetPoint(point, relativeTo, relativePoint, xOfs, yOfs-10);
+]]
+local buffBackgroundTextureTop1 = f2:CreateTexture(nil, "ARTWORK");
+buffBackgroundTextureTop1:SetTexture("Interface\\RAIDFRAME\\UI-ReadyCheckFrame");
+buffBackgroundTextureTop1:SetWidth(250);
+buffBackgroundTextureTop1:SetHeight(5);
+buffBackgroundTextureTop1:SetTexCoord(0.12,0.59,0.07,0.12);
+buffBackgroundTextureTop1:SetPoint("TOPLEFT", 0,-5);
+local buffBackgroundTextureCenter = f2:CreateTexture(nil, "BACKGROUND");
+buffBackgroundTextureCenter:SetTexture("Interface\\RAIDFRAME\\UI-ReadyCheckFrame");
+buffBackgroundTextureCenter:SetSize(250, 20);
+buffBackgroundTextureCenter:SetTexCoord(0.2,0.5,0.1,0.7);
+buffBackgroundTextureCenter:SetPoint("TOPLEFT", 0, -10);
+local buffBackgroundTextureBottom1 = f2:CreateTexture(nil, "ARTWORK");
+buffBackgroundTextureBottom1:SetTexture("Interface\\RAIDFRAME\\UI-ReadyCheckFrame");
+buffBackgroundTextureBottom1:SetWidth(250);
+buffBackgroundTextureBottom1:SetHeight(5);
+buffBackgroundTextureBottom1:SetTexCoord(0.05,0.6,0.66,0.71);
+buffBackgroundTextureBottom1:SetPoint("BOTTOMLEFT", 0, -5);
+local buffBackgroundTextureLeft = f2:CreateTexture(nil, "ARTWORK");
+buffBackgroundTextureLeft:SetTexture("Interface\\RAIDFRAME\\UI-ReadyCheckFrame");
+buffBackgroundTextureLeft:SetWidth(3);
+buffBackgroundTextureLeft:SetHeight(22);
+buffBackgroundTextureLeft:SetTexCoord(0.61,0.62,0.15,0.64);
+buffBackgroundTextureLeft:SetPoint("TOPLEFT", -1, -8);
+local buffBackgroundTextureRight = f2:CreateTexture(nil, "ARTWORK");
+buffBackgroundTextureRight:SetTexture("Interface\\RAIDFRAME\\UI-ReadyCheckFrame");
+buffBackgroundTextureRight:SetWidth(3);
+buffBackgroundTextureRight:SetHeight(22);
+buffBackgroundTextureRight:SetTexCoord(0.61,0.62,0.15,0.64);
+buffBackgroundTextureRight:SetPoint("TOPRIGHT", 0, -8);
+
+local rcText = f2:CreateFontString(nil, "ARTWORK", "GameFontNormal");
+rcText:SetPoint("TOPLEFT", 0, -15);
+rcText:SetJustifyV("TOP");
+rcText:SetJustifyH("CENTER");
+rcText:SetFont("Fonts\\FRIZQT__.TTF", 8.5);
+rcText:SetText("");
+rcText:SetSize(f2:GetWidth(), f2:GetHeight());
+
+local text2 = ReadyCheckListenerFrame:CreateFontString(nil, "ARTWORK", "GameFontNormal");
+text2:SetPoint("TOP", ReadyCheckFrameText, "TOP", 0, -40);
+text2:SetFont("Fonts\\FRIZQT__.TTF", 12);
+text2:SetJustifyH("CENTER");
+local text3 = ReadyCheckListenerFrame:CreateFontString(nil, "ARTWORK", "GameFontNormal");
+text3:SetPoint("BOTTOM", text2, "BOTTOM", 0, -18);
+text3:SetFont("Fonts\\FRIZQT__.TTF", 12);
+text3:SetJustifyH("CENTER");
+ReadyCheckFrame:HookScript("OnHide", function() f2:Hide() end);
+
+
+
 local f = CreateFrame("Frame");
 local flasks = {307185,307187, 307166};
 local oilsIDs = {6188, 6190, 6200};
-local oilIconIDs = {[6188] = 463543, [6190] = 463544, [6200] = 3528422}; --3528423
+local oilIconIDs = {
+	[6188] = 463543, 
+	[6190] = 463544, 
+	[6200] = 3528422
+}; --3528423
 local RED = "\124cFFFF0000";
 local YELLOW = "\124cFFFFFF00";
 local GREEN = "\124cFF00FF00";
@@ -9,6 +90,8 @@ local CROSS = "\124TInterface\\addons\\EndlessRaidTools\\Res\\cross:16\124t";
 local CHECK = "\124TInterface\\addons\\EndlessRaidTools\\Res\\check:16\124t";
 local rcSender = "";
 local raiders = {};
+
+local armorKitSlots = {"ChestSlot", "LegsSlot", "HandsSlot", "FeetSlot"};
 local buffSpellIDs = {
 	["MAGE"] = 1459, 
 	["PRIEST"] = 21562, 
@@ -19,6 +102,96 @@ local buffIconIDs = {
 	["PRIEST"] = 135987, 
 	["WARRIOR"] = 132333,
 };
+
+local scanTooltip = CreateFrame("GameToolTip", "EnRT_TempToolTip", nil, "GameTooltipTemplate");
+scanTooltip:SetOwner(WorldFrame, "ANCHOR_NONE");
+scanTooltip:AddFontStrings(
+    scanTooltip:CreateFontString("$parentTextLeft1", nil, "GameTooltipText"),
+    scanTooltip:CreateFontString("$parentTextRight1", nil, "GameTooltipText")
+);
+
+function armorKit()
+	local count = 0;
+	local shortest = nil;
+	for i = 1, #armorKitSlots do
+		local slotID = select(1, GetInventorySlotInfo(armorKitSlots[i]));
+		scanTooltip:ClearLines();
+		local hasItem = scanTooltip:SetInventoryItem("player", slotID);
+		if (hasItem) then
+			local lines = {scanTooltip:GetRegions()};
+			for index, region in pairs(lines) do
+				if (region and region:GetObjectType() == "FontString") then
+					local text = region:GetText() or "";
+					if (text:match("Reinforced %(%+48 Stamina%)")) then
+						count = count + 1;
+						local timeUnit = text:reverse():match(".*%)"):reverse();
+						local duration = tonumber(text:reverse():match("%d+"):reverse());
+						if (timeUnit:match("hours")) then
+							if (shortest == nil) then
+								shortest = 61;
+							end
+						elseif(timeUnit:match("hour")) then
+							if (shortest == nil) then
+								shortest = 60;
+							elseif (shortest > 60) then
+								shortest = 60;
+							end
+						elseif(timeUnit:match("min")) then
+							if (shortest == nil) then
+								shortest = duration;
+							elseif (shortest > duration) then
+								shortest = duration;
+							end
+						else
+							if (shortest == nil) then
+								shortest = 61;
+							end
+						end
+						--[[
+						for number in text:gmatch("(%d+)") do
+							if (tonumber(number) ~= 48) then
+								if (shortest == nil) then
+									shortest = tonumber(number);
+								else
+									if (shortest > tonumber(number)) then
+										shortest = tonumber(number);
+									end
+								end
+							end
+						end
+						if (shortest == nil) then
+							shortest = 48;
+						end
+						]]
+					end
+				end
+			end
+		end
+	end
+	if (count < 4) then
+		count = RED .. count .. "/4|r ";
+	elseif (count == 4) then
+		count = GREEN .. count .. "/4|r ";
+	end
+	if (shortest == nil) then
+		shortest = "";
+	else
+		if (shortest == 61) then
+			shortest = GREEN .. "2hrs|r";
+		elseif (shortest > 15) then
+			shortest = GREEN .. shortest .. "min|r";
+		elseif (shortest <= 15 and shortest > 8) then
+			shortest = YELLOW .. shortest .. "min|r";
+		elseif (shortest <= 8) then
+			shortest = RED .. shortest .. "min|r";
+		end
+	end
+	print(count)
+	print(shortest)
+	return count, shortest;
+	--3528447
+	--Reinforced (+48 Stamina)
+end
 
 f:RegisterEvent("PLAYER_LOGIN");
 f:RegisterEvent("READY_CHECK");
@@ -40,6 +213,8 @@ local function updateConsumables()
 	end
 	local food, foodIcon, _, _, _, foodTime = EnRT_UnitBuff("player", GetSpellInfo(297039)); -- Random Well Fed Buff
 	local rune, runeIcon, _, _, _, runeTime = EnRT_UnitBuff("player", GetSpellInfo(270058));
+	local armorKitCount, armorKitTime = armorKit();
+	local armorKitIcon = 3528447;
 	flaskIcon = flaskIcon and flaskIcon or 2057568;
 	oilIcon = oilIcon and oilIcon or 463543;
 	foodIcon = foodIcon and foodIcon or 136000;
@@ -58,29 +233,28 @@ local function updateConsumables()
 	oilTime = oilTime and math.floor(tonumber(oilTime)/1000/60) or nil;
 	if (flaskTime) then
 		if (flaskTime > 15) then
-			flaskTime = GREEN .. flaskTime .. "min";
+			flaskTime = GREEN .. flaskTime .. "min|r";
 		elseif (flaskTime <= 15 and flaskTime > 8) then
-			flaskTime = YELLOW .. flaskTime .. "min";
+			flaskTime = YELLOW .. flaskTime .. "min|r";
 		elseif (flaskTime <= 8) then
-			flaskTime = RED .. flaskTime .. "min";
+			flaskTime = RED .. flaskTime .. "min|r";
 		end
 	else
 		flaskTime = CROSS;
 	end
 	if (oilTime) then
 		if (oilTime > 15) then
-			oilTime = GREEN .. oilTime .. "min";
+			oilTime = GREEN .. oilTime .. "min|r";
 		elseif (oilTime <= 15 and oilTime > 8) then
-			oilTime = YELLOW .. oilTime .. "min";
+			oilTime = YELLOW .. oilTime .. "min|r";
 		elseif (oilTime <= 8) then
-			oilTime = RED .. oilTime .. "min";
+			oilTime = RED .. oilTime .. "min|r";
 		end
 	else
 		oilTime = CROSS;
 	end
 	local class = select(2, UnitClass("player"));
 	if (class == "MAGE" or class == "PRIEST" or class == "WARRIOR") then
-		ReadyCheckFrameText:SetSize(280, 40);
 		local count = 0;
 		local total = 0;
 		local unit = nil;
@@ -109,9 +283,28 @@ local function updateConsumables()
 				count = count + 1;
 			end
 		end
-		ReadyCheckFrameText:SetText(blizzText .. "\n\n\124T".. flaskIcon .. ":16\124t" .. flaskTime .. " \124T" .. oilIcon .. ":16\124t" .. oilTime .. " \124T" .. foodIcon .. ":16\124t" .. (food and CHECK or CROSS) .. " \124T" .. runeIcon .. ":16\124t" .. (rune and CHECK or CROSS) .. " \124T" .. buffIconIDs[class] .. ":16\124t" .. (count == total and (GREEN .. count .. "/" .. total) or (RED .. count .. "/" .. total)));
+		if (ReadyCheckFrame.backdrop and ReadyCheckFrame.backdrop.backdropInfo and ReadyCheckFrame.backdrop.backdropInfo.bgFile and ReadyCheckFrame.backdrop.backdropInfo.bgFile:match("ElvUI")) then
+			ReadyCheckFrameText:SetSize(320, 40);
+			ReadyCheckFrameText:SetText(blizzText .. "\n\n\124T".. flaskIcon .. ":16\124t" .. flaskTime .. " \124T" .. oilIcon .. ":16\124t" .. oilTime .. " \124T" .. armorKitIcon .. ":16\124t" .. armorKitCount .. armorKitTime .. " \124T" .. foodIcon .. ":16\124t" .. (food and CHECK or CROSS) .. " \124T" .. runeIcon .. ":16\124t" .. (rune and CHECK or CROSS) .. " \124T" .. buffIconIDs[class] .. ":16\124t" .. (count == total and (GREEN .. count .. "/" .. total) or (RED .. count .. "/" .. total)));
+		else
+			f2:SetPoint("BOTTOM", ReadyCheckFrame, "BOTTOM", 0, -15);
+			f2:Show();
+			rcText:SetText("\124T" .. flaskIcon .. ":16\124t" .. flaskTime .. " \124T" .. oilIcon .. ":16\124t" .. oilTime .. " \124T" .. armorKitIcon .. ":16\124t" .. armorKitCount .. armorKitTime .. " \124T" .. foodIcon .. ":16\124t" .. (food and CHECK or CROSS) .. " \124T" .. runeIcon .. ":16\124t" .. (rune and CHECK or CROSS) .. " \124T" .. buffIconIDs[class] .. ":16\124t" .. (count == total and (GREEN .. count .. "/" .. total) or (RED .. count .. "/" .. total)));
+		end
+		--ReadyCheckFrameText:SetText(blizzText);
+		--text2:SetText("\124T" .. flaskIcon .. ":16\124t" .. flaskTime .. " \124T" .. oilIcon .. ":16\124t" .. oilTime .. "  \124T" .. armorKitIcon .. ":16\124t" .. armorKitCount .. armorKitTime);
+		--text3:SetText("\124T" .. foodIcon .. ":16\124t" .. (food and CHECK or CROSS) .. " \124T" .. runeIcon .. ":16\124t" .. (rune and CHECK or CROSS) .. " \124T" .. buffIconIDs[class] .. ":16\124t" .. (count == total and (GREEN .. count .. "/" .. total) or (RED .. count .. "/" .. total)));
+		--rcText:SetText("\124T" .. flaskIcon .. ":16\124t" .. flaskTime .. " \124T" .. oilIcon .. ":16\124t" .. oilTime .. " \124T" .. armorKitIcon .. ":16\124t" .. armorKitCount .. armorKitTime .. " \124T" .. foodIcon .. ":16\124t" .. (food and CHECK or CROSS) .. " \124T" .. runeIcon .. ":16\124t" .. (rune and CHECK or CROSS) .. " \124T" .. buffIconIDs[class] .. ":16\124t" .. (count == total and (GREEN .. count .. "/" .. total) or (RED .. count .. "/" .. total)));
 	else
-		ReadyCheckFrameText:SetText(blizzText .. "\n\n\124T".. flaskIcon .. ":16\124t" .. flaskTime .. " \124T" .. oilIcon .. ":16\124t" .. oilTime .. " \124T" .. foodIcon .. ":16\124t " .. (food and CHECK or CROSS) .. " \124T" .. runeIcon .. ":16\124t" .. (rune and CHECK or CROSS)); 
+		if (ReadyCheckFrame.backdrop and ReadyCheckFrame.backdrop.backdropInfo and ReadyCheckFrame.backdrop.backdropInfo.bgFile and ReadyCheckFrame.backdrop.backdropInfo.bgFile:match("ElvUI")) then
+			ReadyCheckFrameText:SetSize(320, 40);
+			ReadyCheckFrameText:SetText(blizzText .. "\n\n\124T".. flaskIcon .. ":16\124t" .. flaskTime .. " \124T" .. oilIcon .. ":16\124t" .. oilTime .. " \124T" .. armorKitIcon .. ":16\124t" .. armorKitCount .. armorKitTime .. " \124T" .. foodIcon .. ":16\124t" .. (food and CHECK or CROSS) .. " \124T" .. runeIcon .. ":16\124t" .. (rune and CHECK or CROSS));
+		else
+			f2:SetPoint("BOTTOM", ReadyCheckFrame, "BOTTOM", 0, -15);
+			f2:Show();
+			rcText:SetText("\124T" .. flaskIcon .. ":16\124t" .. flaskTime .. " \124T" .. oilIcon .. ":16\124t" .. oilTime .. " \124T" .. armorKitIcon .. ":16\124t" .. armorKitCount .. armorKitTime .. " \124T" .. foodIcon .. ":16\124t" .. (food and CHECK or CROSS) .. " \124T" .. runeIcon .. ":16\124t" .. (rune and CHECK or CROSS));
+		end
+		--ReadyCheckFrameText:SetText(blizzText .. "\n\n\124T".. flaskIcon .. ":16\124t" .. flaskTime .. " \124T" .. oilIcon .. ":16\124t" .. oilTime .. " \124T" .. foodIcon .. ":16\124t" .. (food and CHECK or CROSS) .. " \124T" .. runeIcon .. ":16\124t" .. (rune and CHECK or CROSS)); 
 	end
 end
 
