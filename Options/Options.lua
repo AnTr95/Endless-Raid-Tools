4a1e0b4974ea938547b0c74c5ef315c1234e08f7
Ant <anton.ronsjo@gmail.com>
2020-09-12 23:02:20 +0200

    changed name of consumablechecks and updated options to match new version

--- Options/Options.lua
+++ Options/Options.lua
@@ -1,16 +1,12 @@
 local L = EnRTLocals;
 
-EnRT_Options = CreateFrame("Frame", "EnRT_OptionsFrame", InterefaceOptionsFramePanelContainer)
+EnRT_Options = CreateFrame("Frame", "EnRT_OptionsFrame", InterfaceOptionsFramePanelContainer)
 EnRT_Options.name = "Endless Raid Tools"
 EnRT_Options:Hide()
 
 local title = EnRT_Options:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
 title:SetPoint("TOP", 0, -16)
 title:SetText(L.OPTIONS_TITLE)
-	
-local tabinfo = EnRT_Options:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
-tabinfo:SetPoint("TOPLEFT", 16, -16)
-tabinfo:SetText(L.OPTIONS_READYCHECK_TITLE)
 
 local author = EnRT_Options:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 author:SetPoint("TOPLEFT", 450, -20)
@@ -27,7 +23,7 @@ end);
 InterfaceOptions_AddCategory(EnRT_Options)
 
 EnRT_GeneralModules = CreateFrame("Frame", "EnRT_GeneralModulesFrame")
-EnRT_GeneralModules.name = "|cFFFFFF00General Modules"
+EnRT_GeneralModules.name = "|cFFFFFF00General Modules|r"
 EnRT_GeneralModules.parent = "Endless Raid Tools"
 EnRT_GeneralModules:SetScript("OnShow", function(EnRT_GeneralModules)
 	InterfaceOptionsFrame_OpenToCategory(EnRT_GeneralOptions)
